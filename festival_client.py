f117dba5a690401770a3b0e07d19325fb71ecab0
Thomas J. Webb <thomas.webb@zyamusic.com>
2015-12-08 13:04:06 -0800

    Generating already decoded strings, not bytes

--- festival_client.py
+++ festival_client.py
@@ -110,7 +110,7 @@ class FestivalClient(object):
             if state == 'get_scheme':
                 current_scheme_responses = data_dump.split(newline_bytes)
                 current_scheme_responses.remove(festival_key)
-                scheme_responses += current_scheme_responses
+                scheme_responses += [response.decode('ISO-8859-1') for response in current_scheme_responses]
             elif state == 'get_wav':
                 audio_responses.append(data_dump.replace(festival_key, b''))
             break
